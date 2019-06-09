6d83ed5b5944860c86046aaf0f047666eaf289d1
Lucien Bill <lucien.bill@gmail.com>
2019-06-09 19:55:48 +0200

    Update lib_messages.js

--- lib_messages.js
+++ lib_messages.js
@@ -1,34 +1,9 @@
 // this  array contains the messages to be displayed
 var messages = [
     {
-        "title":"L'homme qui tombe à pic !",
+        "title":"Billy ?",
         "message":
-            "Sur Uncharted, Billy tombe en moyenne 1 fois toutes les 8,4 minutes, contre 1 fois toutes les 5,6 minutes sur Lego Star Wars"
-    },
-    {
-        "title":"Soldat d'élite",
-        "message":
-            "La stratégie préférée de Billy : assassiner discrètement 3 ennemis, se faire repérer, paniquer puis tirer partout."
-    },
-    {
-        "title":"Headshot ! 🔫",
-        "message":
-            "Et oui Billy sait faire des headshot ! Il en est aussi étonné que vous !"
-    },
-    {
-        "title":"Perdu ?",
-        "message":
-            "Billy s'est une fois perdu dans sa propre résidence. Dans la vraie vie, ce n'est donc pas plus brillant que sur ce jeu..."
-    },
-    {
-        "title":"\"Le son est bon ?\" 🔊",
-        "message":
-            "Cette question est posée en moyenne 3,14 fois par stream. Tapez \"!Billy\" dans le chat, vous verrez !"
-    },
-    {
-        "title":"\"Je suis votre bouclier !\"",
-        "message":
-            "Avez vous déjà vu un stream de Véva avec son Escouade d'Elite sur Overwatch ? Le tank qui fait des blagues nulles, c'est Billy !"
+            "I like Billy. I mean, who doesn't?"
     },
     {
         "title":"Billy multifonctions 🔧",
@@ -36,28 +11,13 @@ var messages = [
             "Mais qui a donc codé ces messages qui s'affichent de temps en temps ? Billy, avec ses 10 doigts !"
     },
     {
-        "title":"Fortnite ?",
+        "title":"Pardon my french",
         "message":
-            "Nope ! Billy ne joue pas à Fortnite. Demandez donc plutôt à Imodium : https://www.twitch.tv/imodium_"
-    },
-    {
-        "title":"Il a pas dit Bonjour",
-        "message":
-            "Dites bonjour à WizeBot, il vous répondra !"
-    },
+            "What? Billy is french? I don't want to use this 'Baguette' technology!"
+    }
     {
         "title":"Piou Piou 🐦",
         "message":
             "On a un twitter : @allgeekstudio"
-    },
-    {
-        "title":"Tex et la gastronomie asiatique",
-        "message":
-            "Tapez '!tex' dans le chat."
-    },
-    {
-        "title":"Alors, heureux ? 🍆",
-        "message":
-            "Tapez '!afflux' dans le chat pour manifester votre satisfaction"
     },
 ]
